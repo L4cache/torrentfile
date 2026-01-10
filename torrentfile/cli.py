5a393c48f270eaa77949f5d568dc3efc312e40b6
L4cache <42721712+L4cache@users.noreply.github.com>
2026-01-10 22:04:59 +0800

    add --public to edit command

--- torrentfile/cli.py
+++ torrentfile/cli.py
@@ -481,6 +481,13 @@ def execute(args: List[str] = None) -> List[str]:
     )
 
     edit_parser.add_argument(
+        "--public",
+        action="store_true",
+        help="make torrent public",
+        dest="public",
+    )
+
+    edit_parser.add_argument(
         "--comment",
         help="replaces any existing comment with <comment>",
         metavar="<comment>",
--- torrentfile/commands.py
+++ torrentfile/commands.py
@@ -283,6 +283,7 @@ def edit(args: Namespace) -> str:
         "announce": args.announce,
         "source": args.source,
         "private": args.private,
+        "public": args.public,
         "comment": args.comment,
     }
     return edit_torrent(metafile, editargs)
