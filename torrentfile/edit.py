f8eebac25df4f3aab17b162b19f2392dd72001f8
L4cache <42721712+L4cache@users.noreply.github.com>
2026-05-31 17:42:54 +0800

    edit: pass empty string to `pop` the trackers

--- torrentfile/edit.py
+++ torrentfile/edit.py
@@ -99,18 +99,22 @@ def edit_torrent(metafile: str, args: dict) -> dict:
 
     if "announce" in args:
         val = args.get("announce", None)
-        if isinstance(val, str):
-            val = val.split()
-        if isinstance(val, list):
-            if val[0].startswith("+"):
-                val[0] = val[0][1:]
-            meta["announce"] = val[0]
-            meta["announce-list"] = []
-            for i in val:
-                if i.startswith("+"):
-                    meta["announce-list"][-1].append(i[1:])
-                else:
-                    meta["announce-list"].append([i])
+        if val == "" or val == [""]:
+            meta.pop("announce", None)
+            meta.pop("announce-list", None)
+        else:
+            if isinstance(val, str):
+                val = val.split()
+            if isinstance(val, list):
+                if val[0].startswith("+"):
+                    val[0] = val[0][1:]
+                meta["announce"] = val[0]
+                meta["announce-list"] = []
+                for i in val:
+                    if i.startswith("+"):
+                        meta["announce-list"][-1].append(i[1:])
+                    else:
+                        meta["announce-list"].append([i])
 
     if "url-list" in args:
         val = args.get("url-list")
